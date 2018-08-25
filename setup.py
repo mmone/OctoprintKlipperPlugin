6ee1c81da952abf93dd68ad84988e3a17d2b65cb
mmone <github@mmone.de>
2018-08-25 17:05:11 +0200

    [release] Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ plugin_package = "octoprint_klipper"
 
 plugin_name = "OctoKlipper"
 
-plugin_version = "0.2.4"
+plugin_version = "0.2.5"
 
 plugin_description = """A plugin for OctoPrint to configure,control and monitor the Klipper 3D printer software."""
 
